47b6bbcc50dcb8681b07dbeab2fd0eb0b81fdb97
Angel Aragon Canovas <aaragon@covalto.com>
2025-06-20 23:00:57 +0000

    ahora necesito que el mnenu de navegación  lo elimines, ya que por cuand

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -5,7 +5,6 @@ import React from 'react';
 import Link from 'next/link';
 
 import { CovaltoLogo } from '@/components/icons/covalto-logo';
-import { MainNav } from './main-nav';
 import { Settings } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
@@ -38,13 +37,8 @@ export default function AppLayout({ children }: AppLayoutProps) {
         </div>
       </header>
 
-      <div className="w-full max-w-screen-xl mx-auto px-6 flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
-        <aside className="fixed top-16 z-30 -ml-2 hidden h-[calc(100vh-4rem)] w-full shrink-0 md:sticky md:block">
-            <div className="h-full py-6 pr-6 lg:py-8">
-              <MainNav />
-            </div>
-        </aside>
-        <main className="flex-1 py-6 lg:py-8">
+      <div className="w-full max-w-screen-xl mx-auto px-6 flex-1">
+        <main className="py-6 lg:py-8">
           {children}
         </main>
       </div>
